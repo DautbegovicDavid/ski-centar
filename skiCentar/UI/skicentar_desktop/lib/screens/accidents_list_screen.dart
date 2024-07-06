8f8660689209b8e432e5098b9b1888d82352537a
DautbegovicDavid <55717097+DautbegovicDavid@users.noreply.github.com>
2024-07-06 21:26:48 +0200

    added reports on ski accident screen

--- skiCentar/UI/skicentar_desktop/lib/screens/accidents_list_screen.dart
+++ skiCentar/UI/skicentar_desktop/lib/screens/accidents_list_screen.dart
@@ -13,6 +13,9 @@ import 'package:skicentar_desktop/providers/ski_accident_provider.dart';
 import 'package:skicentar_desktop/providers/trail_provider.dart';
 import 'package:skicentar_desktop/providers/user_provider.dart';
 import 'package:skicentar_desktop/screens/accidents_add_screen.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:printing/printing.dart';
 
 class AccidentsListScreen extends StatefulWidget {
   const AccidentsListScreen({super.key});
@@ -34,15 +37,45 @@ class _AccidentsListScreenState extends State<AccidentsListScreen> {
 
   final DateFormat formatter = DateFormat('dd MMM yyyy HH:mm');
 
+  final Map<String, bool> _selectedColumns = {
+    'Id': true,
+    'Date': true,
+    'Active': true,
+    'People involved': true,
+    'Reported by': true,
+    'Reporter injured': true,
+  };
+
   @override
   Widget build(BuildContext context) {
     return MasterScreen(
-        "Accidents",
-        Column(
-          children: [_buildSearch(), _buildResultView()],
-        ),
-        false,
-        false);
+      "Accidents",
+      Column(
+        children: [
+          _buildSearch(),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                const Spacer(),
+                const Text("GENERATE REPORT"),
+                IconButton(
+                  icon: const Icon(Icons.settings),
+                  onPressed: _showColumnSelectionDialog,
+                ),
+                IconButton(
+                  icon: const Icon(Icons.picture_as_pdf),
+                  onPressed: _generatePdfReport,
+                ),
+              ],
+            ),
+          ),
+          _buildResultView(),
+        ],
+      ),
+      false,
+      false,
+    );
   }
 
   @override
@@ -58,6 +91,44 @@ class _AccidentsListScreenState extends State<AccidentsListScreen> {
     if (mounted) {
       setState(() {});
     }
+  }
+
+  void _showColumnSelectionDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Select Columns'),
+          content: SingleChildScrollView(
+            child: StatefulBuilder(
+              builder: (BuildContext context, StateSetter setState) {
+                return Column(
+                  children: _selectedColumns.keys.map((String key) {
+                    return CheckboxListTile(
+                      title: Text(key),
+                      value: _selectedColumns[key],
+                      onChanged: (bool? value) {
+                        setState(() {
+                          _selectedColumns[key] = value ?? false;
+                        });
+                      },
+                    );
+                  }).toList(),
+                );
+              },
+            ),
+          ),
+          actions: [
+            TextButton(
+              child: const Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   Widget _buildSearch() {
@@ -149,7 +220,9 @@ class _AccidentsListScreenState extends State<AccidentsListScreen> {
       rows: result?.result
               .map((m) => DataRow(cells: [
                     DataCell(Text(m.id.toString())),
-                    DataCell(Text(m.timestamp != null ?  formatter.format(m.timestamp!) : 'No data')),
+                    DataCell(Text(m.timestamp != null
+                        ? formatter.format(m.timestamp!)
+                        : 'No data')),
                     DataCell(Text(m.isActive! ? "Yes" : "No")),
                     DataCell(Text(m.peopleInvolved?.toString() ?? 'No data')),
                     DataCell(Text(m.user?.userDetails != null &&
@@ -169,5 +242,67 @@ class _AccidentsListScreenState extends State<AccidentsListScreen> {
               .cast<DataRow>() ??
           [],
     );
+  }
+
+  Future<void> _generatePdfReport() async {
+    final pdf = pw.Document();
+
+    final image = await imageFromAssetBundle('assets/images/logo.png');
+
+    final selectedHeaders = _selectedColumns.entries
+        .where((entry) => entry.value)
+        .map((entry) => entry.key)
+        .toList();
+
+    final selectedData = result?.result.map((m) {
+          final data = [];
+          if (_selectedColumns['Id'] ?? false) data.add(m.id.toString());
+          if (_selectedColumns['Date'] ?? false) {
+            data.add(m.timestamp != null
+                ? formatter.format(m.timestamp!)
+                : 'No data');
+          }
+          if (_selectedColumns['Active'] ?? false) {
+            data.add(m.isActive! ? "Yes" : "No");
+          }
+          if (_selectedColumns['People involved'] ?? false) {
+            data.add(m.peopleInvolved?.toString() ?? 'No data');
+          }
+          if (_selectedColumns['Reported by'] ?? false) {
+            data.add(m.user?.userDetails != null &&
+                    m.user?.userDetails?.name != null &&
+                    m.user?.userDetails?.lastName != null
+                ? '${m.user?.userDetails!.name} ${m.user?.userDetails!.lastName}'
+                : m.user?.email ?? 'No email');
+          }
+          if (_selectedColumns['Reporter injured'] ?? false) {
+            data.add(m.isReporterInjured! ? "Yes" : "No");
+          }
+          return data;
+        }).toList() ??
+        [];
+
+    pdf.addPage(
+      pw.Page(
+        build: (pw.Context context) {
+          return pw.Column(
+            children: [
+              pw.Image(image, width: 100, height: 100), // Add the image here
+              pw.SizedBox(height: 20),
+              pw.Text('Accidents Report',
+                  style: const pw.TextStyle(fontSize: 24)),
+              pw.SizedBox(height: 20),
+              pw.TableHelper.fromTextArray(
+                headers: selectedHeaders,
+                data: selectedData,
+              ),
+            ],
+          );
+        },
+      ),
+    );
+
+    await Printing.layoutPdf(
+        onLayout: (PdfPageFormat format) async => pdf.save());
   }
 }
