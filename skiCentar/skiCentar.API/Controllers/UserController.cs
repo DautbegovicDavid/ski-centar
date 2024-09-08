d165f32b0d54e0475121b62da008928f4d87b91a
daut <dautbeg@outlook.com>
2024-09-08 22:13:04 +0200

    removed unused models

--- skiCentar/skiCentar.API/Controllers/UserController.cs
+++ skiCentar/skiCentar.API/Controllers/UserController.cs
@@ -30,13 +30,11 @@ namespace skiCentar.API.Controllers
         [HttpGet("info")]
         public dynamic GetUserInfo()
         {
-            // Retrieve the JWT token from the request header
             string token = Request.Headers["Authorization"].ToString().Replace("Bearer ", "");
 
             var tokenHandler = new JwtSecurityTokenHandler();
             var jwtToken = tokenHandler.ReadJwtToken(token);
 
-            // Retrieve the user ID claim from the token
             Claim userIdClaim = jwtToken.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier);
 
             if (userIdClaim != null)
@@ -44,15 +42,12 @@ namespace skiCentar.API.Controllers
                 int userId;
                 if (int.TryParse(userIdClaim.Value, out userId))
                 {
-                    // Use the user ID to fetch the user details from the database
                     Model.User user = (_service as IUserService).GetById(userId);
 
-                    // Return the user details in the response
                     return Ok(user);
                 }
             }
 
-            // If user ID claim is not found or parsing failed, return an error response
             return BadRequest("Invalid token or user ID not found.");
         }
     }
--- skiCentar/skiCentar.Model/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace skiCentar.Model
-{
-    public class Class1
-    {
-
-    }
-}
