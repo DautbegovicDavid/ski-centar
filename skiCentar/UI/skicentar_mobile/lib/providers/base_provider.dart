1684df0d296b620881ee6ab87296ab1211a8bd54
David Daut <dautbeg@outlook.com>
2024-09-09 22:30:13 +0200

    mobile app default port change

--- skiCentar/UI/skicentar_mobile/lib/providers/base_provider.dart
+++ skiCentar/UI/skicentar_mobile/lib/providers/base_provider.dart
@@ -12,7 +12,7 @@ abstract class BaseProvider<T> with ChangeNotifier {
   BaseProvider(String endpoint) {
     _endpoint = endpoint;
     baseUrl = const String.fromEnvironment("baseUrl",
-        defaultValue: "http://10.0.2.2:5160/api/");
+        defaultValue: "http://10.0.2.2:49153/api/");
   }
 
   Future<SearchResult<T>> get({dynamic filter}) async {
